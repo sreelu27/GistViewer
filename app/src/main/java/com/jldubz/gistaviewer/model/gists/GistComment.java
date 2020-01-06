ff5909f50c94bc4981c35b8e44defa74d51d6a87
sgeetha2 <sgeetha2@its.jnj.com>
2020-01-06 13:55:45 +0000

    GitComment data model updated

--- app/src/main/java/com/jldubz/gistaviewer/model/gists/GistComment.java
+++ app/src/main/java/com/jldubz/gistaviewer/model/gists/GistComment.java
@@ -19,5 +19,49 @@ public class GistComment {
       }
     */
 
-    
+    private int id;
+    private String body;
+    private GitHubUser user;
+    private Date created_at;
+    private Date updated_at;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public GitHubUser getUser() {
+        return user;
+    }
+
+    public void setUser(GitHubUser user) {
+        this.user = user;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
 }
