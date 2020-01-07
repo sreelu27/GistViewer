9f727e614193ebe23d7249207de1e8a4091371e7
sgeetha2 <sgeetha2@its.jnj.com>
2020-01-07 12:01:21 +0000

    Changed the IGitHubService interface methods to include query parameters

--- app/src/main/java/com/jldubz/gistaviewer/model/data/IGitHubService.java
+++ app/src/main/java/com/jldubz/gistaviewer/model/data/IGitHubService.java
@@ -6,17 +6,18 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface IGitHubService {
 
     @GET("/gists/public")
         //https://api.github.com/gists/public
-    Call<List<Gist>> getPublicGists();
+    Call<List<Gist>> getPublicGists(@Query("page") int pageNum);
 
     @GET("/gists")
-    Call<List<Gist>> getYourGists();
+    Call<List<Gist>> getYourGists(@Query("page") int pageNum);
 
     @GET("/gists/starred")
-    Call<List<Gist>> getStarredGists();
+    Call<List<Gist>> getStarredGists(@Query("page") int pageNum);
 
 }
